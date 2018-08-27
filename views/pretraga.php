8e4ec3391ce6616ba9db732d4acb8876d02fd905
jankomarko <markokinez84@gmail.com>
2018-08-27 15:37:21 +0200

    readme updated

---
+++ views/pretraga.php
@@ -0,0 +1,20 @@
+
+    <h1 style="text-align: center"> Pretraga clanova
+        <h1>
+            <form action="../index.php?opcija=pretraga" method="post">
+                <h6 align="center"> Ime i prezime:
+                    <h5>
+                        <h6 align="center"><input type="text" name="ime" value=""></h6>
+                        <h6 align="center"> korisnicko ime:
+                            <h5>
+                                <h6 align="center"><input type="text" name="korisnicko" value=""></h6>
+                                <h6 align="center"><input type="submit" name="action" value="Pronadji"></h6>
+            </form>
+
+
+    <?php
+    if (isset($_POST['action'])) {
+
+        require_once("clanovi.php");
+    }
+;
